2072f89118c8e8da89a2eb466bd42720d0aadc34
Michael <me@chichunov.ru>
2025-05-28 16:39:31 +0000

    app & db connection

--- employee_service/app/app.py
+++ employee_service/app/app.py
@@ -40,3 +40,7 @@ def add_employee():
     except Exception as e:
         conn.rollback()
         return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    
--- employee_service/app/db_config.py
+++ employee_service/app/db_config.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_CONFIG = {
-    'host': 'localhost',
-    'port': os.getenv('POSTGRES_PORT', '5432'),
-    'user': os.getenv('POSTGRES_USER', 'postgres'),
-    'password': os.getenv('POSTGRES_PASSWORD', ''),
-    'database': os.getenv('POSTGRES_DB', 'postgres')
+    'host': 'postgres',
+    'port': os.getenv('POSTGRES_PORT'),
+    'user': os.getenv('POSTGRES_USER'),
+    'password': os.getenv('POSTGRES_PASSWORD'),
+    'database': os.getenv('POSTGRES_DB')
 }
